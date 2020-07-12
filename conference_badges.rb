61dafa6125a6bcc293ee1c35b6d9fb3552fd9610
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-12 22:41:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+    speaker_badge = []
+      attendees.each do |speaker|
+        speaker_badge << "Hello, my name is #{speaker}."
+  end
+  return speaker_badge
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |name, index|
+  room_assignments << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end  
+      return room_assignments
+end
+
+def printer(attendees)
+  print_badge = []
+    print_badge << batch_badge_creator(attendees)
+    puts print_badge.each do |badges|
+      end
+  assign = []
+    assign << assign_rooms(attendees)
+    puts assign.each do |assign|
+      end
+end
