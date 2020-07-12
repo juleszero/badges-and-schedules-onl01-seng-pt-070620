30698dcf385f0244fa3ed52a31f4b6a739b2519b
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-12 23:22:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,8 +21,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees).each
+  puts batch_badge_creator(attendees).each do |badge|
       end
-  puts assign_rooms(attendees).each
+  puts assign_rooms(attendees).each do |rooms|
 end
 end
